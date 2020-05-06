ee6386e92c158e7111eac104d3b1938a92b052d2
omkar <om.kumbhar1998@gmail.com>
2020-05-06 12:24:28 +0530

    added permission request

--- app/src/main/java/com/code_crawler/artisticme/Activity/HomeActivity.java
+++ app/src/main/java/com/code_crawler/artisticme/Activity/HomeActivity.java
@@ -1,46 +1,161 @@
 package com.code_crawler.artisticme.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
 import com.code_crawler.artisticme.Adapter.GridAdapter;
 import com.code_crawler.artisticme.R;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class HomeActivity extends AppCompatActivity {
     String homeDirectoryPath ;
     GridView gridView;
+    GridAdapter gridAdapter;
+    private static final int REQUEST_WRITE_PERMISSION = 2712;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         homeDirectoryPath = Environment.getExternalStorageDirectory()+"/Artwork";
-
         gridView = (GridView) findViewById(R.id.gridView);
-
-
         Intent intent = getIntent();
-
         String name = intent.getStringExtra("name");
         String email = intent.getStringExtra("email");
-
         Toast.makeText(this, name +" has this email "+ email, Toast.LENGTH_SHORT).show();
+
+
+        //Request Permission
+        int permForWrite = ContextCompat.checkSelfPermission( this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int permForRead = ContextCompat.checkSelfPermission( this, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if( permForWrite != PackageManager.PERMISSION_GRANTED && permForRead != PackageManager.PERMISSION_GRANTED ){
+            requestPermission();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         String [] foldernmaes = { "Calligraphy","Digital","Painting","Carricature"};
 
+        ArrayList<File> folderPaths   = SelectFiles();
 
-        GridAdapter gridAdapter = new GridAdapter(this,foldernmaes);
+
+
+        gridAdapter = new GridAdapter(this,foldernmaes);
 
         gridView.setAdapter(gridAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(HomeActivity.this, "cliked "+ position, Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
 
 
 
     }
+
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            ActivityCompat.requestPermissions(HomeActivity.this,
+                    new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                            , Manifest.permission.READ_EXTERNAL_STORAGE},REQUEST_WRITE_PERMISSION );
+
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_WRITE_PERMISSION && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            loadFolders(SelectFiles());
+        }
+    }
+
+    private void loadFolders(ArrayList<File> selectFiles) {
+
+
+
+
+    }
+
+    private ArrayList<File> SelectFiles() {
+
+
+        ArrayList<File> mFiles = new ArrayList<File>();
+        File mDirectory;
+
+
+        String folderPath =   homeDirectoryPath;
+        mDirectory = new File(folderPath);
+
+
+        File[] files = mDirectory.listFiles();
+
+
+        if(files == null) {
+            Toast.makeText(this, "User are null", Toast.LENGTH_SHORT).show();
+        }
+
+        if (files != null && files.length > 0) {
+            mFiles.addAll(Arrays.asList(files));
+        }
+
+        //Toast.makeText(this, "Alloted table : "+ files.length, Toast.LENGTH_SHORT).show();
+
+        return  mFiles ;
+
+
+    }
+
+
+
+
+
 }
